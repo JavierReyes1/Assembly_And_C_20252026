8459c5701d46b682777eb49e9cc185d8c8d1a41f
Javier Reyes <c00312829@setu.ie>
2026-04-28 19:47:53 +0100

    Add input/output helper functions, replace TRAP #15 with C library calls

--- Project_II/Calculator/src/calculator.asm
+++ Project_II/Calculator/src/calculator.asm
@@ -17,3 +17,49 @@ extern exit
 
 section .text
 global main
+
+;-------------------------------------------------------
+; HELPER: print null-terminated string in RDI
+;-------------------------------------------------------
+print_string:
+    sub     rsp, 8
+    xor     rax, rax
+    call    printf
+    add     rsp, 8
+    ret
+
+;-------------------------------------------------------
+; HELPER: print integer in EDI
+;-------------------------------------------------------
+print_number:
+    sub     rsp, 8
+    mov     esi, edi
+    lea     rdi, [fmt_int]
+    xor     rax, rax
+    call    printf
+    add     rsp, 8
+    ret
+
+;-------------------------------------------------------
+; HELPER: print newline
+;-------------------------------------------------------
+print_newline:
+    sub     rsp, 8
+    lea     rdi, [CRLF]
+    xor     rax, rax
+    call    printf
+    add     rsp, 8
+    ret
+
+;-------------------------------------------------------
+; HELPER: read integer, returns result in EAX
+;-------------------------------------------------------
+read_number:
+    sub     rsp, 8
+    lea     rdi, [fmt_scan]
+    lea     rsi, [INPUT_BUF]
+    xor     rax, rax
+    call    scanf
+    mov     eax, [INPUT_BUF]
+    add     rsp, 8
+    ret
